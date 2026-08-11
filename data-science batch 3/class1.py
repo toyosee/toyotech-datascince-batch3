924639483a4848951a97a7f3ece0ca35bd9796e2
Elijah Abolaji <tyabolaji@gmail.com>
2026-08-11 14:24:56 +0100

    Materials:

---
+++ data-science batch 3/class1.py
@@ -0,0 +1,38 @@
+# Data Types
+# Definition: Representation of data
+# string, number (int, float, complex, boolean, none)
+
+# greeting = "Hello World"
+# age = 6
+# salary = 567.89
+# isCooking = True
+# type_checker = type(isCooking)
+# print(type_checker)
+
+# Number checker
+# my_var = "6r"
+# if my_var.isdigit():
+#     new_var = float(my_var)
+#     print(new_var)
+# else:
+#     print("Please eneter a valid number")
+
+# Data structures -
+
+# # list
+# books = ["Journey to the center of earth", 45, 56.6, None, True]
+# coordinates = list((22.3, 56.6))
+# coordinates[0] = 13.3
+# print(coordinates)
+
+my_dict = {
+    "name": "Tolu",
+    "Address": "Murtala square",
+    "salary": 5_000_000_000
+}
+
+# salary = my_dict["salary"]
+# print(salary)
+
+collections = ["Journey to the center of earth", 45, 56.6, None, True]
+print(collections)
