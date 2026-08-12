e3fa7305e9f70b7ac9863f86d3a1e2d81d1a1047
Elijah Abolaji <tyabolaji@gmail.com>
2026-08-12 12:53:13 +0100

    Class 3

--- data-science batch 3/class1.py
+++ data-science batch 3/class1.py
@@ -23,16 +23,48 @@
 # books = ["Journey to the center of earth", 45, 56.6, None, True]
 # coordinates = list((22.3, 56.6))
 # coordinates[0] = 13.3
-# print(coordinates)
+# # print(coordinates)
 
-my_dict = {
-    "name": "Tolu",
-    "Address": "Murtala square",
-    "salary": 5_000_000_000
-}
+# my_dict = {
+#     "name": "Tolu",
+#     "Address": "Murtala square",
+#     "salary": 5_000_000_000
+# }
 
-# salary = my_dict["salary"]
-# print(salary)
+# # salary = my_dict["salary"]
+# # print(salary)
 
-collections = ["Journey to the center of earth", 45, 56.6, None, True]
-print(collections)
+# collections = ["Journey to the center of earth", 45, 56.6, None, True]
+# print(collections)
+
+# number = 5.7
+
+# if isinstance(number, int):
+#     print("You win")
+# elif isinstance(number, float):
+#     print("You Loose")
+# else:
+#     print("Please enter a valid number")
+
+# Operators
+
+# Floor divisio //
+# var1 = 5
+# var2 = 7
+
+# compare = var1 != var2
+# # print(compare)
+
+# shoes = 8
+# shoes /= 2
+
+# addy = "barnawa is a place"
+# print(addy.split(" "))
+
+my_mail= " int@yahoo.com  "
+print(my_mail[0:4])
+# splitter = my_mail.split("@")
+# username = splitter[0]
+# domain_name = splitter[1]
+# print("Your username is ",username[0])
+# print("Your domain is ",domain_name)
